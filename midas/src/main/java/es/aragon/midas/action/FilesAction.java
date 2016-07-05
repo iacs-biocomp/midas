57da0ed994e3258c871eba0d571da3a370908d7b
hiberus <hiberus@salud.aragon.es>
2016-07-05 14:19:43 +0200

    FileAction function added

---
+++ midas/src/main/java/es/aragon/midas/action/FilesAction.java
@@ -0,0 +1,335 @@
+package es.aragon.midas.action;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import es.aragon.midas.vo.FileMetadata;
+
+/**
+ * Control de ficheros y navegación por el servidor
+ * @author Jorge Landa
+ */
+public class FilesAction extends MidasActionSupport {
+	static final long serialVersionUID = 1L;
+	
+	//Variables estaticas
+	public static final String FORMAT_DATE_HOUR_SNS = "yyyy-MM-dd-HH-mm-ss";
+    private final String CARPETA_VERSIONES = "versiones";
+    private final String EXTENSION_VERSIONES = ".bak";
+	
+    //Variables de navegación
+	private String path = "/";
+	private InputStream inputStream;
+	private String fileName;
+	
+	//Variables de subida de un fichero
+	private File uploadFile;
+	private String filename;
+	private String contentType;
+	
+	//Variables de listas de contenido del directorio y segmentación en el path
+	private List<FileMetadata> three = new ArrayList<FileMetadata>();
+	private List<String> redirect = new ArrayList<String>();
+	
+	
+	{
+		setGrantRequired("DEVELOPER");
+	}
+	
+	/**
+	 * Redirige a la pagina de listado de Archivos
+	 * @return
+	 * @throws FileNotFoundException 
+	 */
+	public String list() throws FileNotFoundException{
+		setRedirect();
+		String result = setThree();
+		
+		if(result.equals("File")){
+			return "download";
+		}else{
+			return "files";
+		}
+	}
+	
+	/**
+	 * Metodo que se encarga de subir un fichero al servidor
+	 * @return
+	 * @throws IOException 
+	 */
+	public String upload() throws IOException{
+		setRedirect();
+		
+		/** Si el path termina en / lo devuelve integro, sino le añade / */
+		if(!path.endsWith("/")) {
+			path += "/";
+        }
+		
+		//Establece la ruta de las versiones, al path actual le añade la carpeta versiones
+        String rutaVersiones = path + CARPETA_VERSIONES;
+		
+        if(uploadFile != null){
+        	File newFile = new File(path, filename);
+        	/** Si el fichero existe procedemos a crear su .bak*/
+            if(newFile.exists()){
+            	File carpetaVersiones = new File(rutaVersiones);
+            	/** Comprobamos si la carpeta de versiones ya existe, sino se crea*/
+            	if(!carpetaVersiones.exists()){
+            		carpetaVersiones.mkdirs();
+            	}
+            	
+            	//Establece el nuevo nombre para la versión .bak, nombre del fichero + fecha de creación  + .bak
+            	String nuevoNombre = filename + getDateFormatted(getDateNow(), FORMAT_DATE_HOUR_SNS) + EXTENSION_VERSIONES;
+            	
+            	File moveFile = new File(path, nuevoNombre);
+            	try {
+            		//Copia el nuevo fichero al que se va a mover
+            		FileUtils.copyFile(newFile, moveFile);
+            		//Copia el fichero a mover a la ruta de versiones
+    				FileUtils.copyFileToDirectory(moveFile, carpetaVersiones);
+    				//Borra el fichero de la carpeta origen
+    				moveFile.delete();
+    			} catch (IOException e) {
+    				log.error("Error: " + e);
+    				throw e;
+    			}
+            }
+            
+            /** Se intenta copiar el fichero que vamos a subir en el servidor*/
+            try {
+    			FileUtils.copyFile(uploadFile, newFile);	
+    		} catch (IOException e) {
+    			log.error("Error copiando el archivo");
+    			throw e;
+    		}
+            
+            //Recargamos el árbol de contenido del directorio en el que nos encontramos
+            three.clear();
+    		String result = setThree();
+    		if(result.equals("File")){
+    			return "download";
+    		}else{
+    			return "files";
+    		}
+        }
+        /** Control de Error de fichero nulo. Se excluye la acción*/
+        else{
+        	three.clear();
+    		String result = setThree();
+    		if(result.equals("File")){
+    			return "download";
+    		}else{
+    			return "files";
+    		}
+        }
+	}
+	
+	//<!------- Getters and Setters
+	
+	/**
+	 * Devuelve el Path
+	 * 		Si el path termina en / lo devuelve integro
+	 * 		sino le añade /
+	 * @return
+	 */
+	public String getPath(){
+		if(path.endsWith("/")) {
+			return path;
+        }else{
+        	return path + "/";
+        }
+	}
+	
+	/**
+	 * Establece el path
+	 * @param path
+	 */
+	public void setPath(String path){
+		this.path = path;
+	}
+	
+	/**
+	 * Devuelve el resultado del árbol de contenidos de un directorio
+	 * 		establece el arbol de directorio o descarga el fichero
+	 * @return
+	 * @throws FileNotFoundException 
+	 */
+	public String setThree() throws FileNotFoundException{
+		File file = new File(path);
+		
+		/** Si el fichero es un directorio lo recorremos y añadimos su contenido al listado de directorio */
+		if(file.isDirectory()){
+			if(file.listFiles() != null){
+				for (File fichero : file.listFiles()) {
+					if(fichero.isDirectory()){
+						three.add(new FileMetadata("D", fichero.getName(), fichero.length()));
+					}else{
+						three.add(new FileMetadata("F", fichero.getName(), fichero.length()));
+					}
+				}
+			}
+			
+			//Proceso de ordenación de ficheros del directorio
+			Collections.sort(three);
+			
+			return "Directory";
+			
+		}
+		/** Si el fichero es un fichero lo descargamos */
+		else if(file.isFile()){	
+			try {
+				setInputStream(new FileInputStream(file));
+				setFileName(file.getName());
+			} catch (FileNotFoundException e) {
+				throw e;
+			}
+			return "File";
+		}else{
+			return "Directory";
+		}
+	}
+	
+	/**
+	 * Devuelve el arbol de directorios
+	 * @return
+	 */
+	public List<FileMetadata> getThree(){
+		return three;
+	}
+	
+	/**
+	 * Establece el redirect.
+	 * 		Divive el Path según los / y nos permite la navegación por las distintas
+	 *      subdirectorios.
+	 */
+	public void setRedirect(){
+		//Se comprueba si el path es la ruta principal "/"
+		if(path.length() == 1){
+			redirect.add("/");
+		}else{
+			//Divive el path dividido por los "/"
+			String [] splits = path.split("/");
+			for (String split : splits){
+				if(split.equals("")){
+					redirect.add("/");
+				}else{
+					redirect.add(split);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Devuelve el redirect
+	 * @return
+	 */
+	public List<String> getRedirect(){
+		return redirect;
+	}
+
+	/**
+	 * Devuelve el inputStream del fichero
+	 * @return
+	 */
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	/**
+	 * Establece el inputStream del fichero
+	 * @param inputStream
+	 */
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
+	/**
+	 * Devuelve el nombre del fichero seleccionado
+	 * @return
+	 */
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * Establece el nombre del fichero seleccionado
+	 * @param fileName
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	/**
+	 * Devuelve el fichero a subir al servidor
+	 * @return
+	 */
+	public File getUploadFile(){
+		return uploadFile;
+	}
+	
+	/**
+	 * Establece el fichero a subir al servidor
+	 * @param uploadFile
+	 */
+	public void setUploadFile(File uploadFile) {
+		this.uploadFile = uploadFile;
+	}
+	
+	/**
+	 * Establece el nombre del fichero a subir al servidor
+	 * @param filename
+	 */
+	public void setUploadFileFileName(String filename){
+		this.filename = filename;
+	}
+	
+	/**
+	 * Establece el contentType del fichero a subir al servidor
+	 * @param contentType
+	 */
+	public void setUploadFileContentType(String contentType) {
+        this.contentType = contentType;
+     }
+	
+	/**
+	 * Establece el formato "yyyy-MM-dd-HH-mm-ss"
+	 * para la fecha de los archivos .bak cuando subimos un fichero
+	 * @param fecha
+	 * @param formato
+	 * @return
+	 */
+	public static String getDateFormatted(Date fecha, String formato){
+		SimpleDateFormat formatter = new SimpleDateFormat(formato);
+        // En este caso se obtiene la fecha de hoy.
+        String stringDate = null;
+        if (fecha != null) {
+            try {
+                stringDate = formatter.format(fecha);
+            } catch (Exception e) {
+                stringDate = null;
+            }
+        }
+        return stringDate;
+	}
+	
+	/**
+	 * Devuelve la fecha actual del sistema
+	 * @return
+	 */
+    public static Date getDateNow() {
+        GregorianCalendar calendar = new GregorianCalendar();
+        Date currentDate = calendar.getTime();
+        return currentDate;
+    }
+}
---
+++ midas/src/main/java/es/aragon/midas/vo/FileMetadata.java
@@ -0,0 +1,105 @@
+package es.aragon.midas.vo;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+
+/**
+ * Define la estructura de un fichero
+ * @author Jorge Landa
+ *
+ */
+public class FileMetadata implements Comparable<FileMetadata>{
+
+	private String tipo;
+	private String nombre;
+	private BigDecimal size;
+	private BigDecimal bits = new BigDecimal("1024");
+	
+	/**
+	 * Llamada de declaración
+	 */
+	public FileMetadata() {
+		
+	}
+	
+	/**
+	 * Constructor del fichero
+	 * @param tipo
+	 * @param nombre
+	 * @param size
+	 */
+	public FileMetadata(String tipo, String nombre, long size){
+		this.tipo = tipo;
+		this.nombre = nombre;
+		this.size = new BigDecimal(size);
+
+		if(size > 0){
+			this.size = this.size.divide(bits);
+			this.size.setScale(2, BigDecimal.ROUND_HALF_UP);
+		}
+
+	}
+	
+	/**
+	 * Devuelve el tipo de fichero que es
+	 * @return
+	 */
+	public String getTipo(){
+		return this.tipo;
+	}
+	
+	/**
+	 * Establece el tipo de fichero que es
+	 * @param tipo
+	 */
+	public void setTipo(String tipo){
+		this.tipo = tipo;
+	}
+	
+	/**
+	 * Devuelve el nombre del fichero
+	 * @return
+	 */
+	public String getNombre(){
+		return this.nombre;
+	}
+	
+	/**
+	 * Establece el nombre del fichero
+	 * @param nombre
+	 */
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	
+	/**
+	 * Devuelve el tamaño del fichero
+	 * @return
+	 */
+	public BigDecimal getSize(){		
+		return this.size;
+	}
+	
+	/**
+	 * Establece el tamaño del fichero
+	 * @param size
+	 */
+	public void setSize(long size){
+		this.size = new BigDecimal(size);
+		this.size.setScale(2, BigDecimal.ROUND_DOWN);
+	}
+
+	/**
+	 * Ordenación: este metodo nos ayuda a ordenar
+	 * 		la estructura de directorios, primero carpetas luego ficheros.
+	 */
+	public int compareTo(FileMetadata o) {
+		if(tipo.compareTo(o.tipo) > 0)
+			return 1;
+		else if(tipo.compareTo(o.tipo) == 0)
+			return 0;
+		else
+			return -1;
+	}
+}
